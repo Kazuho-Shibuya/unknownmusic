5049fb0c4acb79d806a7a5049a637a313007be47
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-02 14:30:19 +0900

    root追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/new'
+  root 'tops#top'
+  get  '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
