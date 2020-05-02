5293584e434f5eaf1239a59057f74e359f31d4eb
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-02 15:09:49 +0900

    トップページ更新

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = 'UnknownMusic'
+    if page_title.empty?
+      base_title
+    else
+      page_title + '|' + base_title
+    end
+  end
 end
