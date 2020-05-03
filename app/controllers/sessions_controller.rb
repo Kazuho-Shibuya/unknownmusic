799cf088ad1262c4fb9a6b388ae2bbd2dc253e46
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-03 14:23:30 +0900

    ログイン認証機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # ユーザがデータベースにあり、かつ、認証に成功した場合
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが無効です'
+      render 'new'
+    end
   end
+
+  def destroy; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # 保存の成功
+      log_in @user
       flash[:success] = 'ようこそ！未知の音楽領域へ'
       redirect_to @user
     else
