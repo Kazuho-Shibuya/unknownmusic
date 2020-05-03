2425e806fb51b215c75e354ff23a930e3f972105
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-03 12:54:13 +0900

    login page create

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
   root 'tops#top'
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
