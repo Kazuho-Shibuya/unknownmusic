633d077d9a1c0caa7e388e41aac480c24d3e256d
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-03 16:28:05 +0900

    logout correction

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   resources :users
 end
