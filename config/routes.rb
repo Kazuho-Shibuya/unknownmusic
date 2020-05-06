5499c001260a0fb2070c1b4167cd14c7ef072267
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-06 14:24:27 +0900

    ユーザ編集画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   root 'tops#top'
   get  '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
+  get  '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  patch '/users/:id/edit', to: 'users#update'
   resources :users
 end
