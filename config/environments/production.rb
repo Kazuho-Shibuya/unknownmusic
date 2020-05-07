413989cbef9499d38c201084b1f55ac135053860
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-07 18:23:10 +0900

    SendGrid修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'young-dawn-77447.herokuapp.com'
+  host = 'https://young-dawn-77447.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
