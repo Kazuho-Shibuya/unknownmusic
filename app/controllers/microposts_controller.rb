9e083db446e9cc3086d84f3fb1383a5849348cdd
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-11 14:48:46 +0900

    micropost feed create

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -7,15 +8,25 @@ class MicropostsController < ApplicationController
       flash[:success] = 'Micropost created!'
       redirect_to home_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
-  def destroy; end
+  def destroy
+    @micropost.destroy
+    flash[:success] = '投稿は削除されました'
+    redirect_to request.referer || root_url
+  end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
