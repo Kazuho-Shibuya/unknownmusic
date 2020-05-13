e73d9e183390adc2c5ded5f9dd52d5b911156573
Kazuho-Shibuya <businessfive25@gmail.com>
2020-05-13 14:57:32 +0900

    ユーザフォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,7 +76,10 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where('user_id = ?', id)
+    following_ids = "SELECT followed_id FROM relationships
+    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+    OR user_id = :user_id", user_id: id)
   end
 
   # ユーザーをフォローする
