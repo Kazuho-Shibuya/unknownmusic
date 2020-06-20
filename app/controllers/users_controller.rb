0a66a05a0ce19e55be286671038040ef8e456e1a
Kazuho-Shibuya <businessfive25@gmail.com>
2020-06-20 19:44:42 +0900

    ユーザ検索機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.where(activated: true).paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page]).search(params[:search])
   end
 
   def show
