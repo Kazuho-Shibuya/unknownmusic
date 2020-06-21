2418cdceaac7472b802d6d9d2d628716ed61ec0f
Kazuho-Shibuya <businessfive25@gmail.com>
2020-06-21 09:44:54 +0900

    ホーム画面に投稿検索機能を追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   before_action :set_micropost, only: %i[home search_result]
 
-  def home; end
+  def home
+    @micropost  = current_user.microposts.build
+    # 検索拡張機能として.search(params[:search])を追加
+    @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
+  end
 
   def search_result
     @result_artist_name = params[:artist]
