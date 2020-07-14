af6842156d7f5760690bb8dd8ea30410067e5cf3
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-15 00:22:47 +0900

    Userモデル有効スペック追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   # 名前、メール、パスワードがあれば有効な状態であること
-  it 'is valid with a name, email, and password'
+  it 'is valid with a name, email, and password' do
+    user = User.new(
+      name: 'Aaron Summer',
+      email: 'tester@example.com',
+      password: 'foobar'
+    )
+    expect(user).to be_valid
+  end
+
   # 名前がなければ無効な状態であること
   it 'is invalid without a name'
   # メールアドレスがなければ無効な状態であること
