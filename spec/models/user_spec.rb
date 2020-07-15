6fedaa06bd7ce28e6efda30774f507ff608f53d9
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-15 18:20:52 +0900

    Userモデルのバリデーションテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   # 名前、メール、パスワードがあれば有効な状態であること
   it 'is valid with a name, email, and password' do
     user = User.new(
-      name: 'Aaron Summer',
+      name: '佐藤太郎',
       email: 'tester@example.com',
       password: 'foobar'
     )
@@ -12,9 +12,30 @@ RSpec.describe User, type: :model do
   end
 
   # 名前がなければ無効な状態であること
-  it 'is invalid without a name'
+  it 'is invalid without a name' do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include('を入力してください')
+  end
   # メールアドレスがなければ無効な状態であること
-  it 'is invalid without an email address'
+  it 'is invalid without an email address' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include('を入力してください')
+  end
   # 重複したメールアドレスなら無効な状態であること
-  it 'is invalid with a duplicate email address'
+  it 'is invalid with a duplicate email address' do
+    user = User.create(
+      name: '佐藤太郎',
+      email: 'tester@example.com',
+      password: 'foobar'
+    )
+    user = User.new(
+      name: '佐藤太郎',
+      email: 'tester@example.com',
+      password: 'foobar'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('はすでに存在します')
+  end
 end
