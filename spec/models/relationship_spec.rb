4c3f714beab332770bdef4b11f46f10f06d30495
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-18 18:57:18 +0900

    Relationshipモデルスペックの更新

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  it 'relationshipが作成できる'
-  it 'follower_idがないとrelationshipが作成できない'
-  it 'followed_idがないとrelationshipが作成できない'
+  it 'relationshipが作成できる' do
+    user = User.create(
+      id: '1',
+      name: '佐藤太郎',
+      email: 'tester@example.com',
+      password: 'password'
+    )
+    other_user = User.create(
+      id: '2',
+      name: '田中圭介',
+      email: 'tester2@example.com',
+      password: 'password'
+    )
+    relationship = user.active_relationships.create!(followed_id: other_user.id)
+    expect(relationship).to be_valid
+  end
+  it 'follower_idがないとrelationshipが作成できない' do
+    relationship = Relationship.new(follower_id: nil)
+    relationship.valid?
+    expect(relationship.errors[:follower_id]).to include('を入力してください')
+  end
+  it 'followed_idがないとrelationshipが作成できない' do
+    relationship = Relationship.new(followed_id: nil)
+    relationship.valid?
+    expect(relationship.errors[:followed_id]).to include('を入力してください')
+  end
 end
