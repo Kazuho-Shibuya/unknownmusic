ee6c988f9690681ad961717fbd6c1da8c737325b
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-19 18:43:26 +0900

    Userモデルのファクトリにシーケンスを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name '佐藤太郎'
-    email 'tester@example.com'
+    sequence(:email) { |_n| "tester#{n}@example.com" }
     password 'password'
   end
 end
