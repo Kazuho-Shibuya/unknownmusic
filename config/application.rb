123179645db4e12ceb819cde04f67ad804589b1b
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-19 11:52:06 +0900

    Factory Bot追加

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,6 @@ module UnknownMusic
 
     config.generators do |g|
       g.test_framework :rspec,
-                       fixtures: false,
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false
