58e8335e41615c8da4a65cf7b80fa2a434a35510
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-19 18:49:10 +0900

    Userファクトリの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name '佐藤太郎'
-    sequence(:email) { |_n| "tester#{n}@example.com" }
+    sequence(:email) { |n| "tester#{n}@example.com" }
     password 'password'
   end
 end
