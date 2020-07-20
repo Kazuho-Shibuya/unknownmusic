b71fd7fcd43d6f1bf726578d674bb19db630597e
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-20 12:22:49 +0900

    micropostモデルファクトリの更新

--- spec/factories/microposts.rb
+++ spec/factories/microposts.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :micropost do
-    
+    content '是非聴いてください'
+    song 'Love so sweet'
+    artist '嵐'
+    listening_url 'https://p.scdn.co/mp3-preview/49846ff4faf7a04f86aeab2b9dc3fd29f18da9e0?cid=94ea968e9d334bd3be33c1c3fbb25cc0'
+    association :user
   end
 end
