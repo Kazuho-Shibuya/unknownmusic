4ad0d91b931ead6f3d2362f340879d2dc5da3ed0
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-20 16:13:11 +0900

    Micropostモデルスペック更新

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,72 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  it 'ユーザID、内容、曲、アーティスト、試聴URLがあれば投稿できる' do
-    user = User.new(
-      id: '1',
-      name: '佐藤太郎',
-      email: 'tester@example.com',
-      password: 'password'
-    )
-
-    micropost = user.microposts.build(
-      content: '是非聴いてください',
-      song: 'Love so sweet',
-      artist: '嵐',
-      listening_url: 'https://p.scdn.co/mp3-preview/49846ff4faf7a04f86aeab2b9dc3fd29f18da9e0?cid=94ea968e9d334bd3be33c1c3fbb25cc0'
-    )
-    expect(micropost).to be_valid
+  it '内容、曲、アーティスト、試聴URLがあれば投稿できる' do
+    expect(FactoryBot.build(:micropost)).to be_valid
   end
 
   it 'ユーザが紐づけられていないと投稿できない' do
-    micropost = Micropost.new(user_id: nil)
+    micropost = FactoryBot.build(:micropost, user_id: nil)
     micropost.valid?
     expect(micropost.errors[:user_id]).to include('を入力してください')
   end
 
   it '内容が空欄だと投稿できない' do
-    micropost = Micropost.new(content: nil)
+    micropost = FactoryBot.build(:micropost, content: nil)
     micropost.valid?
     expect(micropost.errors[:content]).to include('を入力してください')
   end
 
   it '曲が空欄だと投稿できない' do
-    micropost = Micropost.new(song: nil)
+    micropost = FactoryBot.build(:micropost, song: nil)
     micropost.valid?
     expect(micropost.errors[:song]).to include('を入力してください')
   end
 
   it 'アーティストが空欄だと投稿できない' do
-    micropost = Micropost.new(artist: nil)
+    micropost = FactoryBot.build(:micropost, artist: nil)
     micropost.valid?
     expect(micropost.errors[:artist]).to include('を入力してください')
   end
 
   it '試聴URLが空欄だと投稿できない' do
-    micropost = Micropost.new(listening_url: nil)
+    micropost = FactoryBot.build(:micropost, listening_url: nil)
     micropost.valid?
     expect(micropost.errors[:listening_url]).to include('を入力してください')
   end
 
   it '内容が100文字以内の場合投稿できる' do
-    user = User.new(
-      id: '1',
-      name: '佐藤太郎',
-      email: 'tester@example.com',
-      password: 'password'
-    )
-
-    micropost = user.microposts.build(
-      content: 'a' * 100,
-      song: 'Love so sweet',
-      artist: '嵐',
-      listening_url: 'https://p.scdn.co/mp3-preview/49846ff4faf7a04f86aeab2b9dc3fd29f18da9e0?cid=94ea968e9d334bd3be33c1c3fbb25cc0'
-    )
+    micropost = FactoryBot.build(:micropost, content: 'a' * 100)
     expect(micropost).to be_valid
   end
 
   it '内容が101文字以上の場合投稿できない' do
-    micropost = Micropost.new(content: 'a' * 101)
+    micropost = FactoryBot.build(:micropost, content: 'a' * 101)
     micropost.valid?
     expect(micropost.errors[:content]).to include('は100文字以内で入力してください')
   end
