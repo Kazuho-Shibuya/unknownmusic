1bcb8c817f287d90c5ec82ffd19de74b68416955
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-21 19:26:49 +0900

    staticpagesコントローラスペックの更新

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe '#home' do
-    it '正常にレスポンスを返すこと' do
+    it 'ログインしていない場合ステータスが302であること' do
       get :home
-      expect(response).to eq have_http_status '200'
+      expect(response.status).to eq 302
+    end
+    it 'ログインしていない場合ログインページに遷移することを確認' do
+      get :home
+      expect(response).to redirect_to login_url
     end
   end
 end
