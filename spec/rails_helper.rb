15c4d2caa7a03b0fa6fbd3500586d3180da4363f
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-27 20:45:08 +0900

    ログインマクロ読み込み

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,8 @@ require 'database_cleaner'
 
 require 'capybara/rspec'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -32,6 +34,7 @@ require 'capybara/rspec'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include LoginMacros
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
