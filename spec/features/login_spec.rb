7919c34f6fbf63814b3578077d94126c8ef0ee4b
Kazuho-Shibuya <businessfive25@gmail.com>
2020-07-27 19:12:22 +0900

    loginスペックの修正

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -13,6 +13,6 @@ RSpec.feature 'Login', type: :feature do
     fill_in 'パスワード', with: user.password
     click_button 'ログイン'
 
-    expect(current_path).to eq '/users/1'
+    expect(current_path).to eq user_path(user)
   end
 end
