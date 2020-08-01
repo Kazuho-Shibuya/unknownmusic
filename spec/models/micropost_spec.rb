904e4cde4d6fb125728759d85d82a4f75201e907
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-01 19:20:23 +0900

    Micropostモデルスペック更新

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,48 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  it '内容、曲、アーティスト、試聴URLがあれば投稿できる' do
-    expect(FactoryBot.build(:micropost)).to be_valid
+  context '内容、曲、アーティスト、試聴URLがある場合' do
+    it '投稿できる' do
+      expect(FactoryBot.build(:micropost)).to be_valid
+    end
   end
 
-  it 'ユーザが紐づけられていないと投稿できない' do
-    micropost = FactoryBot.build(:micropost, user_id: nil)
-    micropost.valid?
-    expect(micropost.errors[:user_id]).to include('を入力してください')
+  context 'ユーザが紐づけられていない場合' do
+    it '投稿できない' do
+      micropost = FactoryBot.build(:micropost, user_id: nil)
+      micropost.valid?
+      expect(micropost.errors[:user_id]).to include('を入力してください')
+    end
   end
 
-  it '内容が空欄だと投稿できない' do
-    micropost = FactoryBot.build(:micropost, content: nil)
-    micropost.valid?
-    expect(micropost.errors[:content]).to include('を入力してください')
+  context '内容が空欄の場合' do
+    it '投稿できない' do
+      micropost = FactoryBot.build(:micropost, content: nil)
+      micropost.valid?
+      expect(micropost.errors[:content]).to include('を入力してください')
+    end
   end
 
-  it '曲が空欄だと投稿できない' do
-    micropost = FactoryBot.build(:micropost, song: nil)
-    micropost.valid?
-    expect(micropost.errors[:song]).to include('を入力してください')
+  context '曲が空欄の場合' do
+    it '投稿できない' do
+      micropost = FactoryBot.build(:micropost, song: nil)
+      micropost.valid?
+      expect(micropost.errors[:song]).to include('を入力してください')
+    end
   end
 
-  it 'アーティストが空欄だと投稿できない' do
-    micropost = FactoryBot.build(:micropost, artist: nil)
-    micropost.valid?
-    expect(micropost.errors[:artist]).to include('を入力してください')
+  context 'アーティストが空欄の場合' do
+    it '投稿できない' do
+      micropost = FactoryBot.build(:micropost, artist: nil)
+      micropost.valid?
+      expect(micropost.errors[:artist]).to include('を入力してください')
+    end
   end
 
-  it '試聴URLが空欄だと投稿できない' do
-    micropost = FactoryBot.build(:micropost, listening_url: nil)
-    micropost.valid?
-    expect(micropost.errors[:listening_url]).to include('を入力してください')
+  context '試聴URLが空欄の場合' do
+    it '投稿できない' do
+      micropost = FactoryBot.build(:micropost, listening_url: nil)
+      micropost.valid?
+      expect(micropost.errors[:listening_url]).to include('を入力してください')
+    end
   end
 
-  it '内容が100文字以内の場合投稿できる' do
-    micropost = FactoryBot.build(:micropost, content: 'a' * 100)
-    expect(micropost).to be_valid
+  context '内容が100文字以内の場合' do
+    it '投稿できる' do
+      micropost = FactoryBot.build(:micropost, content: 'a' * 100)
+      expect(micropost).to be_valid
+    end
   end
 
-  it '内容が101文字以上の場合投稿できない' do
-    micropost = FactoryBot.build(:micropost, content: 'a' * 101)
-    micropost.valid?
-    expect(micropost.errors[:content]).to include('は100文字以内で入力してください')
+  context '内容が101文字以上の場合' do
+    it '投稿できない' do
+      micropost = FactoryBot.build(:micropost, content: 'a' * 101)
+      micropost.valid?
+      expect(micropost.errors[:content]).to include('は100文字以内で入力してください')
+    end
   end
 end
