955226d81984ac88bb67b428ca4b3c77d7761e82
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-01 19:31:02 +0900

    Relationshipモデルスペック更新

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,20 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  it 'relationshipが作成できる' do
-    user1 = FactoryBot.create(:user)
-    user2 = FactoryBot.create(:user)
-    expect(FactoryBot.build(:relationship, follower_id: user1.id, followed_id: user2.id)).to be_valid
+  let(:user) { FactoryBot.create(:user) }
+  context 'フォローユーザとフォロワーユーザがいる場合' do
+    it 'フォローフォロワーの関係が作成できる' do
+      followed_user = user
+      follower_user = user
+      relationship = FactoryBot.build(:relationship, followed_id: followed_user.id, follower_id: follower_user.id)
+      expect(relationship).to be_valid
+    end
   end
 
-  it 'follower_idがないとrelationshipが作成できない' do
-    relationship = FactoryBot.build(:relationship, follower_id: nil)
-    relationship.valid?
-    expect(relationship.errors[:follower_id]).to include('を入力してください')
+  context 'フォローユーザがいない場合' do
+    it 'フォローフォロワーの関係が作成できない' do
+      relationship = FactoryBot.build(:relationship, followed_id: nil)
+      relationship.valid?
+      expect(relationship.errors[:followed_id]).to include('を入力してください')
+    end
   end
-  it 'followed_idがないとrelationshipが作成できない' do
-    relationship = FactoryBot.build(:relationship, followed_id: nil)
-    relationship.valid?
-    expect(relationship.errors[:followed_id]).to include('を入力してください')
+
+  context 'フォロワーユーザがいない場合' do
+    it 'フォローフォロワーの関係が作成できない' do
+      relationship = FactoryBot.build(:relationship, follower_id: nil)
+      relationship.valid?
+      expect(relationship.errors[:follower_id]).to include('を入力してください')
+    end
   end
 end
