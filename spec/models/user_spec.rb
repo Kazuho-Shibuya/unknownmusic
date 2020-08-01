09ecdeef25a2f791169fce11b0faa3e65f1a1f0f
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-01 19:13:27 +0900

    Userモデルスペック更新

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,79 +1,103 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it '名前、メール、パスワードがあれば登録できる' do
-    expect(FactoryBot.build(:user)).to be_valid
+  context '名前、メール、パスワードがある場合' do
+    it '登録できる' do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
   end
 
-  it '名前が空欄だと登録できない' do
-    user = FactoryBot.build(:user, name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include('を入力してください')
+  context '名前が空欄の場合' do
+    it '登録できない' do
+      user = FactoryBot.build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include('を入力してください')
+    end
   end
 
-  it 'メールアドレスが空欄だと登録できない' do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include('を入力してください')
+  context 'メールアドレスが空欄の場合' do
+    it '登録できない' do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include('を入力してください')
+    end
   end
 
-  it 'パスワードが空欄だと登録できない' do
-    user = FactoryBot.build(:user, password: nil)
-    user.valid?
-    expect(user.errors[:password]).to include('を入力してください')
+  context 'パスワードが空欄の場合' do
+    it '登録できない' do
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include('を入力してください')
+    end
   end
 
-  it '登録済みのメールアドレスは登録できない' do
-    FactoryBot.create(:user, email: 'tester@example.com')
-    user = FactoryBot.build(:user, email: 'tester@example.com')
-    user.valid?
-    expect(user.errors[:email]).to include('はすでに存在します')
+  context '登録済みのメールアドレスの場合' do
+    it '登録できない' do
+      FactoryBot.create(:user, email: 'tester@example.com')
+      user = FactoryBot.build(:user, email: 'tester@example.com')
+      user.valid?
+      expect(user.errors[:email]).to include('はすでに存在します')
+    end
   end
 
-  it 'パスワードが6文字以上の場合登録できる' do
-    user = FactoryBot.build(:user, password: 'foobar')
-    user.valid?
-    expect(user).to be_valid
+  context 'パスワードが6文字以上の場合' do
+    it '登録できる' do
+      user = FactoryBot.build(:user, password: 'foobar')
+      user.valid?
+      expect(user).to be_valid
+    end
   end
 
-  it 'パスワードが5文字以下の場合登録できない' do
-    user = FactoryBot.build(:user, password: 'fooba')
-    user.valid?
-    expect(user.errors[:password]).to include('は6文字以上で入力してください')
+  context 'パスワードが5文字以下の場合' do
+    it '登録できない' do
+      user = FactoryBot.build(:user, password: 'fooba')
+      user.valid?
+      expect(user.errors[:password]).to include('は6文字以上で入力してください')
+    end
   end
 
-  it '名前が50文字以下の場合登録できる' do
-    user = FactoryBot.build(:user, name: 'a' * 50)
-    user.valid?
-    expect(user).to be_valid
+  context '名前が50文字以下の場合' do
+    it '登録できる' do
+      user = FactoryBot.build(:user, name: 'a' * 50)
+      user.valid?
+      expect(user).to be_valid
+    end
   end
 
-  it '名前が51文字以上の場合登録できない' do
-    user = FactoryBot.build(:user, name: 'a' * 51)
-    user.valid?
-    expect(user.errors[:name]).to include('は50文字以内で入力してください')
+  context '名前が51文字以上の場合' do
+    it '登録できない' do
+      user = FactoryBot.build(:user, name: 'a' * 51)
+      user.valid?
+      expect(user.errors[:name]).to include('は50文字以内で入力してください')
+    end
+  end
+  context 'メールアドレスが255文字以下の場合' do
+    it '登録できる' do
+      user = FactoryBot.build(:user, email: 'a' * 243 + '@example.com')
+      user.valid?
+      expect(user).to be_valid
+    end
   end
 
-  it 'メールアドレスが255文字以下の場合登録できる' do
-    user = FactoryBot.build(:user, email: 'a' * 243 + '@example.com')
-    user.valid?
-    expect(user).to be_valid
+  context 'メールアドレスが256文字以上の場合' do
+    it '登録できない' do
+      user = FactoryBot.build(:user, email: 'a' * 244 + '@example.com')
+      user.valid?
+      expect(user.errors[:email]).to include('は255文字以内で入力してください')
+    end
   end
 
-  it 'メールアドレスが256文字以上の場合登録できない' do
-    user = FactoryBot.build(:user, email: 'a' * 244 + '@example.com')
-    user.valid?
-    expect(user.errors[:email]).to include('は255文字以内で入力してください')
+  context 'パスワードと確認が一致していない場合' do
+    it '登録できない' do
+      user = FactoryBot.build(:user, password_confirmation: 'foobar')
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include('とパスワードの入力が一致しません')
+    end
   end
-
-  it 'パスワードと確認が一致していないと登録できない' do
-    user = FactoryBot.build(:user, password_confirmation: 'foobar')
-    user.valid?
-    expect(user.errors[:password_confirmation]).to include('とパスワードの入力が一致しません')
-  end
-
-  it 'パスワードが暗号化されているか' do
-    user = FactoryBot.create(:user, email: 'tester@example.com')
-    expect(user.password_digest).to_not eq 'foobar'
+  context '登録済みの場合' do
+    it 'パスワードが暗号化されている' do
+      user = FactoryBot.create(:user, email: 'tester@example.com')
+      expect(user.password_digest).to_not eq 'foobar'
+    end
   end
 end
