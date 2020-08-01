d3b530b5adb28189a6b5ab8fd1dd61c04df15323
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-01 19:52:31 +0900

    Likeモデルスペック更新

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,19 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  it 'likeが作成できる' do
-    expect(FactoryBot.build(:like)).to be_valid
+  context 'ユーザと投稿が紐づけられている場合' do
+    it 'いいねを作成できる' do
+      expect(FactoryBot.build(:like)).to be_valid
+    end
   end
 
-  it 'ユーザが紐づけられていないといいねできない' do
-    like = FactoryBot.build(:like, user_id: nil)
-    like.valid?
-    expect(like.errors[:user_id]).to include('を入力してください')
+  context 'ユーザが紐づけられていない場合' do
+    it 'いいねを作成できない' do
+      like = FactoryBot.build(:like, user_id: nil)
+      like.valid?
+      expect(like.errors[:user_id]).to include('を入力してください')
+    end
   end
 
-  it 'マイクロポストが紐づけられていないといいねできない' do
-    like = FactoryBot.build(:like, micropost_id: nil)
-    like.valid?
-    expect(like.errors[:micropost_id]).to include('を入力してください')
+  context '投稿が紐づけられていない場合' do
+    it 'いいねを作成できない' do
+      like = FactoryBot.build(:like, micropost_id: nil)
+      like.valid?
+      expect(like.errors[:micropost_id]).to include('を入力してください')
+    end
   end
 end
