a50fd54afc2b58fb411f30bf8ecc60db21a34ca6
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-02 11:45:43 +0900

    static_pages_controllerスペック更新

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,37 +2,43 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe '#home' do
-    it 'ログインページに遷移すること' do
+    before do
       get :home
+    end
+
+    it 'ログインページに遷移すること' do
       expect(response).to redirect_to login_url
     end
 
     it 'ステータスが302であること' do
-      get :home
       expect(response.status).to eq 302
     end
   end
 
   describe '#about' do
-    it '正常にレスポンスを返すこと' do
+    before do
       get :about
+    end
+
+    it '正常にレスポンスを返すこと' do
       expect(response).to be_successful
     end
 
     it 'ステータスが200であること' do
-      get :about
       expect(response.status).to eq 200
     end
   end
 
   describe '#search_result' do
-    it 'ログインページに遷移すること' do
+    before do
       post :search_result
+    end
+
+    it 'ログインページに遷移すること' do
       expect(response).to redirect_to login_url
     end
 
     it 'ステータスが302であること' do
-      post :search_result
       expect(response.status).to eq 302
     end
   end
