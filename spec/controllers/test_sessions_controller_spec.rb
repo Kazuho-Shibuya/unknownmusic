cbfa2000c91ae20ee8a5c8ee96afbcac71f37200
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-02 11:39:55 +0900

    test_sessions_controllerスペック更新

--- spec/controllers/test_sessions_controller_spec.rb
+++ spec/controllers/test_sessions_controller_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe TestSessionsController, type: :controller do
+  before do
+    post :create
+  end
+
   describe '#create' do
     it '正常にレスポンスを返すこと' do
-      post :create
       expect(response).to be_successful
     end
 
     it 'ステータスが204であること' do
-      post :create
       expect(response.status).to eq 204
     end
   end
