11635c2d31ae5ffb10007e2c20fbd96bda070278
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-16 10:57:53 +0900

    開発環境メーラー更新

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
   #
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: 'Account activation'
+    mail to: user.email, subject: 'アカウントの有効化'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -16,6 +16,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: 'Password reset'
+    mail to: user.email, subject: 'パスワードのリセット'
   end
 end
