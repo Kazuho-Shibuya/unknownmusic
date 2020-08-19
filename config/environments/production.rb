2ccbe7e0584265fc8878cab34126c8331b0f8d38
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-19 10:43:23 +0000

    AWS_SES更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :ses
+
+    # ActionMailer Setting with AWS SES
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_caching = true
+  config.action_mailer.default_url_options = { host: 'unknownmusic.net', protocol: 'https' }
+  config.action_mailer.smtp_settings = {
+      :address =>        "email-smtp.ap-northeast-1.amazonaws.com",
+      :port =>           587,
+      :domain =>         ENV['DOMAIN'],
+      :authentication => :login,
+      :user_name =>      ENV['AWS_ACCESS_KEY_ID'],
+      :password =>       ENV['AWS_SECRET_ACCESS_KEY']
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/initializers/aws.rb
+++
@@ -1,5 +0,0 @@
-ActionMailer::Base.add_delivery_method :ses,
-                                       AWS::SES::Base,
-                                       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                                       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                                       server: 'feedback-smtp.ap-northeast-1.amazonses.com'
