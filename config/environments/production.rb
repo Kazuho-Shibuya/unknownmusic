664ef4c8539222706b5057d753807b31a36caf56
Kazuho-Shibuya <businessfive25@gmail.com>
2020-08-19 23:14:05 +0900

    本番環境のメール設定更新

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,17 +70,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-    # ActionMailer Setting with AWS SES
+  # ActionMailer Setting with AWS SES
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = true
   config.action_mailer.default_url_options = { host: 'unknownmusic.net', protocol: 'https' }
   config.action_mailer.smtp_settings = {
-      :address =>        "email-smtp.ap-northeast-1.amazonaws.com",
-      :port =>           587,
-      :domain =>         ENV['DOMAIN'],
-      :authentication => :login,
-      :user_name =>      ENV['AWS_ACCESS_KEY_ID'],
-      :password =>       ENV['AWS_SECRET_ACCESS_KEY']
+    address: 'email-smtp.ap-northeast-1.amazonaws.com',
+    port: 587,
+    domain: ENV['DOMAIN'],
+    authentication: :login,
+    user_name: ENV['AWS_ACCESS_KEY_ID'],
+    password: ENV['AWS_SECRET_ACCESS_KEY']
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
