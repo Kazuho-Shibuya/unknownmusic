8c6cb847701d37ca3dfb5af43b0505bdac1bfe5c
Kazuho-Shibuya <businessfive25@gmail.com>
2020-09-08 04:31:36 +0900

    docker-compose更新

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -2,14 +2,16 @@
   $worker  = 2
   $timeout = 30
   $app_dir = "/var/www/rails/unknownmusic"
-  $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+  $listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
   $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-  $std_log = File.expand_path 'log/unicorn.log', $app_dir
+  $stderr_log = File.expand_path 'log/unicorn_stderr.log', $app_dir
+  $stdout_log = File.expand_path 'log/unicorn_stdout.log', $app_dir
+
   # set config
   worker_processes  $worker
   working_directory $app_dir
-  stderr_path $std_log
-  stdout_path $std_log
+  stderr_path $stderr_log
+  stdout_path $stdout_log
   timeout $timeout
   listen  $listen
   pid $pid
@@ -21,7 +23,8 @@
     old_pid = "#{server.config[:pid]}.oldbin"
     if old_pid != server.pid
       begin
-        Process.kill "QUIT", File.read(old_pid).to_i
+        sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+        Process.kill(sig, File.read(old_pid).to_i)
       rescue Errno::ENOENT, Errno::ESRCH
       end
     end
