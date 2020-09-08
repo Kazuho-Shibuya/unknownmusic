bcfcb985e7c87fc1407734df65664b9001bae8de
Kazuho-Shibuya <businessfive25@gmail.com>
2020-09-08 20:19:26 +0900

    ローカルの変更をコンテナに即時反映設定追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   host = 'samplehost'
   Rails.application.routes.default_url_options[:host] = host
