6cf0ab287feef955111748beb5f067f7aa22d8eb
Kazuho-Shibuya <businessfive25@gmail.com>
2020-10-21 00:00:07 +0900

    パスワード再設定メールのデザイン更新

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,10 +11,10 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      flash[:info] = 'Email sent with password reset instructions'
+      flash[:info] = 'パスワード再設定メールを送信しました。'
       redirect_to root_url
     else
-      flash.now[:danger] = 'Email address not found'
+      flash.now[:danger] = 'メールアドレスが見つかりません。'
       render 'new'
     end
   end
@@ -28,7 +28,7 @@ class PasswordResetsController < ApplicationController
     elsif @user.update(user_params)
       log_in @user
       @user.update_attribute(:reset_digest, nil)
-      flash[:success] = 'パスワードがリセットされました'
+      flash[:success] = 'パスワードがリセットされました。'
       redirect_to @user
     else
       render 'edit'
@@ -58,7 +58,7 @@ class PasswordResetsController < ApplicationController
   def check_expiration
     return unless @user.password_reset_expired?
 
-    flash[:danger] = 'パスワードリセットは期限ぎれです'
+    flash[:danger] = 'パスワードのリセットリンクは期限切れです。'
     redirect_to new_password_reset_url
   end
 
