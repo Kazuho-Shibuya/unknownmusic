e4b033f4f6cc1273369904cb02efc6ab136a4831
Kazuho-Shibuya <businessfive25@gmail.com>
2021-01-28 17:58:59 +0900

    Spotify_apiクラスを追加

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,42 +1,11 @@
 class SongsController < ApplicationController
+  require './app/modules/spotify_api'
+
   def search
     return if params[:search].blank?
 
-    require 'net/http'
-    require 'uri'
-
-    uri = URI.parse('https://accounts.spotify.com/api/token')
-    request = Net::HTTP::Post.new(uri)
-    request['Authorization'] = "Basic #{ENV['CLIENT_ENCODE']}"
-    request.set_form_data(
-      'grant_type' => 'client_credentials'
-    )
-
-    req_options = {
-      use_ssl: uri.scheme == 'https'
-    }
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
-    end
-
-    auth_params = JSON.parse(response.body)
-    access_token = auth_params['access_token']
-
-    get_uri = "https://api.spotify.com/v1/search?q=#{params[:search]}&type=track&market=JP"
-    uri_encode = URI.encode(get_uri)
-    uri = URI.parse(uri_encode)
-    request = Net::HTTP::Get.new(uri)
-    request.content_type = 'application/json'
-    request['Accept'] = 'application/json'
-    request['Authorization'] = "Bearer #{access_token}"
-    request['Accept-Language'] = 'ja;q=1'
-
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
-    end
-
-    response_hash = response.body
-    @search_infomations = JSON.parse(response_hash)
+    spotify_api = Spotify_api.new
+    access_token = spotify_api.get_access_token
+    @search_infomations = spotify_api.search(access_token, params[:search])
   end
 end
