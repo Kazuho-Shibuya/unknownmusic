4d317c6080d9623c071a60856ec7179e328b3a55
Kazuho-Shibuya <businessfive25@gmail.com>
2021-02-07 01:01:56 +0900

    user.rbの宣言順を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,19 @@ class User < ApplicationRecord
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
+  has_secure_password
+
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
+  mount_uploader :image, ImageUploader
+
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
@@ -13,19 +26,6 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :likes, dependent: :destroy
-
-  validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-
-  has_secure_password
-
-  before_save   :downcase_email
-  before_create :create_activation_digest
-
-  mount_uploader :image, ImageUploader
 
   # 渡された文字列のハッシュ値を返す
   def self.digest(string)
