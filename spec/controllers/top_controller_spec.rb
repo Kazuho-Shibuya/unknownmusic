0f0ba40f5a7d841b608cbeafef1ee1e08d9b4234
Kazuho-Shibuya <businessfive25@gmail.com>
2021-03-07 06:11:45 +0900

    static_pages_controller_specのtopアクションをtop_controller_specのindexアクションに修正

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe TopController, type: :controller do
+  describe '#index' do
+    before do
+      get :index
+    end
 
+    it '正常にレスポンスを返すこと' do
+      expect(response).to be_successful
+    end
+
+    it 'ステータスが200であること' do
+      expect(response.status).to eq 200
+    end
+  end
 end
