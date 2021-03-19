152d5b5de152e10bf44193a3a5295c867cd85f3f
Kazuho-Shibuya <businessfive25@gmail.com>
2021-03-20 03:26:58 +0900

    users_controller_specのhomeアクションをhome_controller_specのindexアクションに修正

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
 
+  describe '#index' do
+    before do
+      get :index, params: { id: user.id }
+    end
+
+    it 'ログインページに遷移すること' do
+      expect(response).to redirect_to new_session_url
+    end
+
+    it 'ステータスが302であること' do
+      expect(response.status).to eq 302
+    end
+  end
 end
