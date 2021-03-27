d1147dd41ee9143c65e01b59af18735070482f39
Kazuho-Shibuya <businessfive25@gmail.com>
2021-03-28 04:26:57 +0900

    users_controllerのprivateアクションを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[index destroy
-                                          following followers]
-  before_action :check_admin_user,     only: %i[destroy]
-  before_action :check_test_user, only: %i[destroy]
+  before_action :logged_in_user, only: %i[index following followers]
 
   def index
     @users = User.where(activated: true).page(params[:page]).search(params[:search])
@@ -26,26 +23,5 @@ class UsersController < ApplicationController
     @user  = User.find(params[:id])
     @users = @user.followers.page(params[:page])
     render 'show_follow'
-  end
-
-  private
-
-  # 管理者かどうか確認
-  def check_admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
-
-  # テストユーザかどうか確認する
-  def check_test_user
-    @user = User.find(params[:id])
-    return unless @user.email == 'test@example.com'
-
-    flash[:warning] = 'テストユーザのため変更できません'
-    redirect_to root_url
-  end
-
-  def set_micropost
-    @micropost = current_user.microposts.build
-    @feed_items = current_user.feed.page(params[:page])
   end
 end
