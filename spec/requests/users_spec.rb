fa64f5cfd7fe816827a2a73bdad9e2f3e28f9154
Kazuho-Shibuya <businessfive25@gmail.com>
2021-06-23 01:36:59 +0900

    usersリクエストスペックを追加

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe 'GET #index' do
+  end
+
+  describe 'GET #show' do
+  end
+
+  describe 'GET #following' do
+  end
+
+  describe 'GET #followers' do
+  end
+end
