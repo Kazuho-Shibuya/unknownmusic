2d21fd4718c504524ab89d885d7f281c42c954d9
Kazuho-Shibuya <businessfive25@gmail.com>
2021-06-25 01:27:06 +0900

    controller_specをrequest_specに修正(users_controllerのshowアクション)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,20 +3,6 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   let(:user) { FactoryBot.create(:user) }
 
-  describe '#show' do
-    before do
-      get :show, params: { id: user.id }
-    end
-
-    example '正常にレスポンスを返していること' do
-      expect(response).to be_successful
-    end
-
-    example 'ステータスが200であること' do
-      expect(response.status).to eq 200
-    end
-  end
-
   describe '#following' do
     before do
       get :following, params: { id: user.id }
