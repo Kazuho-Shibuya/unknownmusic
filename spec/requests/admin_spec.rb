d178ba56b78cbfa708929cf792d7975ecb31a98a
Kazuho-Shibuya <businessfive25@gmail.com>
2021-07-02 02:26:00 +0900

    controller_specをrequest_specに修正(admin_controller)

---
+++ spec/requests/admin_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
+  describe 'DELETE #destroy' do
+    before do
+      delete admin_path user.id
+    end
+
+    example 'ログインページにリダイレクトすること' do
+      expect(response).to redirect_to new_session_url
+    end
+
+    example 'ステータスが302であること' do
+      expect(response).to have_http_status(302)
+    end
+  end
+end
