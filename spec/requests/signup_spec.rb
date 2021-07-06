bf76c4bad6abfa36637fdef1f9a93d5cdbd0b6db
Kazuho-Shibuya <businessfive25@gmail.com>
2021-07-07 01:01:51 +0900

    signup_specを修正

--- spec/requests/signup_spec.rb
+++ spec/requests/signup_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Signup', type: :request do
   describe 'GET #new' do
     example 'ステータスが200であること' do
-      get new_session_path
+      get new_signup_path
       expect(response).to have_http_status(200)
     end
   end
