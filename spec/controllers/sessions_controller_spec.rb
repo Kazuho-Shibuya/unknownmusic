4b45e66032e0507b6bba1724b20bae48d7944fdb
Kazuho-Shibuya <businessfive25@gmail.com>
2021-07-08 01:28:48 +0900

    controller_specをrequest_specに修正(sessions_controllerのcreateアクション)

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,20 +3,6 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
   let(:user) { FactoryBot.create(:user) }
 
-  describe '#create' do
-    before do
-      post :create, params: { session: { email: user.email } }
-    end
-
-    example '正常にレスポンスを返すこと' do
-      expect(response).to be_successful
-    end
-
-    example 'ステータスが200であること' do
-      expect(response.status).to eq 200
-    end
-  end
-
   describe '#destroy' do
     before do
       get :destroy, params: { id: user.id }
--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Sessions', type: :request do
+  let(:user) { FactoryBot.create(:user) }
+
   describe 'GET #new' do
     example 'ステータスが200であること' do
       get new_session_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST #create' do
+    example 'ステータスが200であること' do
+      post sessions_path, params: { session: { email: user.email } }
       expect(response).to have_http_status(200)
     end
   end
