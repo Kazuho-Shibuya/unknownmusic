ab4becb91b5a72ce918dfbcb98eeb10dfeb68192
Kazuho-Shibuya <businessfive25@gmail.com>
2021-07-12 01:46:03 +0900

    controller_specをrequest_specに修正(password_resets_controllersのeditアクション)

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -3,20 +3,6 @@ require 'rails_helper'
 RSpec.describe PasswordResetsController, type: :controller do
   let(:user) { FactoryBot.create(:user) }
 
-  describe '#edit' do
-    before do
-      get :edit, params: { id: user.id }
-    end
-
-    example '正常にレスポンスを返さないこと' do
-      expect(response).to_not be_successful
-    end
-
-    example 'ステータスが302であること' do
-      expect(response.status).to eq 302
-    end
-  end
-
   describe '#update' do
     before do
       patch :update, params: { id: user.id }
--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe 'PasswordResets', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET #edit' do
+    before do
+      get edit_password_reset_path user.id
+    end
+
+    example 'トップページにリダイレクトすること' do
+      expect(response).to redirect_to root_url
+    end
+
+    example 'ステータスが302であること' do
+      expect(response).to have_http_status(302)
+    end
+  end
 end
