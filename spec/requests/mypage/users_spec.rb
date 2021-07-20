86dfa059d9b133670e6a9ce56ab1eead68963a84
Kazuho-Shibuya <businessfive25@gmail.com>
2021-07-21 00:29:50 +0900

    controller_specをrequest_specに修正(mypage/users_controllersのupdateアクション)

--- spec/requests/mypage/users_spec.rb
+++ spec/requests/mypage/users_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe 'Mypage/Users', type: :request do
       expect(response).to have_http_status(302)
     end
   end
+
+  describe 'PATCH #update' do
+    before do
+      patch mypage_users_path
+    end
+
+    example 'ログインページにリダイレクトすること' do
+      expect(response).to redirect_to new_session_url
+    end
+
+    example 'ステータスが302であること' do
+      expect(response).to have_http_status(302)
+    end
+  end
 end
