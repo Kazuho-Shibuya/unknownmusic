f0ae35ecb0963f80ba67a69a3fffc1f2021772f0
Kazuho-Shibuya <businessfive25@gmail.com>
2021-07-24 02:48:21 +0900

    controller_specをrequest_specに修正(song/search_controllerのcreateアクション)

--- spec/requests/song/search_spec.rb
+++ spec/requests/song/search_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe 'Song/Search', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST #create' do
+    before do
+      post song_search_index_path
+    end
+
+    example 'ログインページにリダイレクトすること' do
+      expect(response).to redirect_to new_session_url
+    end
+
+    example 'ステータスが302であること' do
+      expect(response).to have_http_status(302)
+    end
+  end
 end
