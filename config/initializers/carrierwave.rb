278e2b3d67e65702d81447ae6845b619088d3c19
Kazuho-Shibuya <businessfive25@gmail.com>
2021-11-22 01:17:51 +0900

    環境変数をcredentials.yml.encに修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
 
-    config.fog_directory = ENV['S3_BUCKET'],
+    config.fog_directory = 'unknownmusic428',
     config.cache_storage = :fog
     config.storage = :fog
   else
