3af760bb987b62b042970ccf55b05ad3007d6881
Kazuho-Shibuya <businessfive25@gmail.com>
2021-12-03 03:07:57 +0900

    mailgunの設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Rails.application.configure do
      :domain => host,
      :authentication => :plain,
   }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
