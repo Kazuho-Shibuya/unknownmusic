b981cf6d68c71a7b02beb80d38581ba40e1e49fe
Kazuho-Shibuya <businessfive25@gmail.com>
2022-05-08 11:24:51 +0900

    不要なメソッドを削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,14 @@
 class HomeController < ApplicationController
   before_action :confirmation_logged_in_user?
-  before_action :set_micropost
+  before_action :build_micropost
 
   def index
-    @feed_items = @feed_items.search(params[:search])
+    @feed_items = current_user.feed.page(params[:page]).search(params[:search])
   end
 
   private
 
-  def set_micropost
+  def build_micropost
     @micropost = current_user.microposts.build
-    @feed_items = current_user.feed.page(params[:page])
   end
 end
